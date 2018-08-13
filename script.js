104986c4ab902b008f32b14e1b3c01f51255319f
Michal Turek <michael.turek@rizk.com>
2018-08-13 14:35:34 +0200

    ajax-requests

---
+++ script.js
@@ -0,0 +1,35 @@
+function createCode(data) {
+  var code = document.createElement("CODE");
+  var t = document.createTextNode(data);
+  code.appendChild(t);
+  document.body.appendChild(code);
+  code.style.display = "block";
+}
+
+function mapData (data) {
+    data.map(x => createCode(x.name));
+}
+
+// Ajax requests
+// vanilla.js
+function AjaxVanillaJs() {
+  var http = new XMLHttpRequest();
+
+  http.onreadystatechange = function() {
+    if (http.readyState == 4 && http.status == 200) {
+      var result = JSON.parse(http.response);
+      mapData(result);
+    }
+  };
+
+  http.open("GET", "https://jsonplaceholder.typicode.com/users", true);
+  http.send();
+}
+
+//jQuery 
+function jQuery () {
+    $.get("https://jsonplaceholder.typicode.com/users", function(data){
+        mapData(data);
+    })
+
+}
